cd2a6f465ed5fbb4cb06c9819dddedb37769828d
Raj Gupta <rgm5823@gmail.com>
2022-09-21 01:29:52 +0530

    deployement

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const connectDB = require('./config/connectDB')
 const productRoutes = require('./routes/product')
 const campaignRoutes = require('./routes/campaign')
+const path = require('path')
 
 const app = express()
 dotenv.config()
@@ -14,9 +15,20 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/campaigns', campaignRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Zocket.')
-})
+if (process.env.MODE === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+//deployment
+__dirname = path.resolve()
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
